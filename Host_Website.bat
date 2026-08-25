33357eabc404457346b8d27e7357a2940c39c7b9
Karo B. Hasan <karobakr.hasan000@gmail.com>
2026-08-25 23:04:01 +0300

    Delete Host_Website.bat

--- Host_Website.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Karo Personal Website Local Host
-echo ==========================================================
-echo  Starting Local Web Server for Karo Personal Website...
-echo  Local URL: http://localhost:8080/
-echo ==========================================================
-start http://localhost:8080/
-powershell -ExecutionPolicy Bypass -File "%~dp0server.ps1"
-pause
